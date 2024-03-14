d4af8f5c0a70217691eb89dcdae45c8369ab6efd
Alex Greene <142196597+Arkaeyuh@users.noreply.github.com>
2024-03-14 16:43:39 -0500

    add Program.cs

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using DSharpPlus;
+
+namespace College_Hotline
+{
+    internal class Program
+    {
+
+        static async Task Main(string[] args)
+        {
+            var jsonReader = new JsonReader();
+            await jsonReader.ReadJson();
+
+            var discordConfig = new DiscordConfiguration()
+            {
+                Intents = DiscordIntents.All,
+                Token = jsonReader.Token,
+                TokenType = TokenType.Bot,
+                AutoReconnect = true
+            };
+
+            var discord = new DiscordClient(discordConfig);
+            
+            discord.MessageCreated += async (s, e) =>
+            {
+                if (e.Message.Content.ToLower().StartsWith("ping"))
+                    await e.Message.RespondAsync("pong!");
+            };
+            
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+            
+
+        }
+    }
+}

d916be242af332440527dee249510ed7f44d7332
Alex Greene <142196597+Arkaeyuh@users.noreply.github.com>
2024-03-14 16:44:07 -0500

    Create JsonReader.cs

---
+++ JsonReader.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+
+namespace College_Hotline;
+
+public class JsonReader
+{
+    public string Token { get; set; }
+    public string Prefix { get; set; }
+
+    public async Task ReadJson()
+    {
+        using (StreamReader sr = new StreamReader("config.json"))
+        {
+            string json = await sr.ReadToEndAsync();
+            JsonStructure data = JsonConvert.DeserializeObject<JsonStructure>(json);
+
+            this.Token = data.Token;
+            this.Prefix = data.Prefix;
+        }
+    }
+}
+
+internal sealed class JsonStructure
+{   
+    public string Token { get; set; }
+    
+    public string Prefix { get; set; }
+}
